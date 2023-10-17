cb3891afa1cc723d0c2193f516effb6f06306ca3
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-10-17 16:46:46 +0900

    modified:   src/App.js 	new file:   src/ui-components/GPTobject.d.ts 	new file:   src/ui-components/GPTobject.jsx 	new file:   src/ui-components/Heads.d.ts 	new file:   src/ui-components/Heads.jsx 	new file:   src/ui-components/PostForm.d.ts 	new file:   src/ui-components/PostForm.jsx

---
+++ src/ui-components/PostForm.jsx
@@ -0,0 +1,426 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Icon, Text, View } from "@aws-amplify/ui-react";
+export default function PostForm(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <View
+      width="300px"
+      height="500px"
+      display="block"
+      gap="unset"
+      alignItems="unset"
+      justifyContent="unset"
+      overflow="hidden"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...getOverrideProps(overrides, "PostForm")}
+      {...rest}
+    >
+      <Text
+        fontFamily="Inter"
+        fontSize="12px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="14.522727012634277px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="17px"
+        left="13px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="title:"
+        {...getOverrideProps(overrides, "title:")}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="19.363636016845703px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="13px"
+        left="50px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="TITLETITLETITLETITLETITLE"
+        {...getOverrideProps(overrides, "TITLETITLETITLETITLETITLE3848778")}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="19.363636016845703px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="40px"
+        left="50px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="TITLETITLETITLETITLETITLE"
+        {...getOverrideProps(overrides, "TITLETITLETITLETITLETITLE3848782")}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="12px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="14.522727012634277px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="43px"
+        left="13px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="tag:"
+        {...getOverrideProps(overrides, "tag:")}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="12px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="14.522727012634277px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="124px"
+        left="13px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="object:"
+        {...getOverrideProps(overrides, "object:")}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="500"
+        color="rgba(0,0,0,1)"
+        lineHeight="19.363636016845703px"
+        textAlign="left"
+        display="block"
+        direction="column"
+        justifyContent="unset"
+        width="unset"
+        height="unset"
+        gap="unset"
+        alignItems="unset"
+        position="absolute"
+        top="145px"
+        left="calc(50% - 137px - 0px)"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&#xA;aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        {...getOverrideProps(
+          overrides,
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        )}
+      ></Text>
+      <Flex
+        gap="0"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-end"
+        position="absolute"
+        top="63px"
+        left="50px"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 8")}
+      >
+        <Text
+          fontFamily="Inter"
+          fontSize="12px"
+          fontWeight="500"
+          color="rgba(0,0,0,1)"
+          lineHeight="14.522727012634277px"
+          textAlign="left"
+          display="block"
+          direction="column"
+          justifyContent="unset"
+          width="unset"
+          height="unset"
+          gap="unset"
+          alignItems="unset"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="aws"
+          {...getOverrideProps(overrides, "aws")}
+        ></Text>
+        <Flex
+          padding="0px 0px 0px 0px"
+          width="12px"
+          height="12px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          shrink="0"
+          position="relative"
+          {...getOverrideProps(overrides, '\uD83E\uDD86 icon "close circle"')}
+        >
+          <Icon
+            width="12px"
+            height="12px"
+            viewBox={{ minX: 0, minY: 0, width: 12, height: 12 }}
+            paths={[
+              {
+                d: "M6 0C2.694 0 0 2.694 0 6C0 9.306 2.694 12 6 12C9.306 12 12 9.306 12 6C12 2.694 9.306 0 6 0ZM8.016 7.38C8.19 7.554 8.19 7.842 8.016 8.016C7.926 8.106 7.812 8.148 7.698 8.148C7.584 8.148 7.47 8.106 7.38 8.016L6 6.636L4.62 8.016C4.53 8.106 4.416 8.148 4.302 8.148C4.188 8.148 4.074 8.106 3.984 8.016C3.81 7.842 3.81 7.554 3.984 7.38L5.364 6L3.984 4.62C3.81 4.446 3.81 4.158 3.984 3.984C4.158 3.81 4.446 3.81 4.62 3.984L6 5.364L7.38 3.984C7.554 3.81 7.842 3.81 8.016 3.984C8.19 4.158 8.19 4.446 8.016 4.62L6.636 6L8.016 7.38Z",
+                fill: "rgba(41,45,50,1)",
+                fillRule: "nonzero",
+              },
+            ]}
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            position="absolute"
+            top="0%"
+            bottom="0%"
+            left="0%"
+            right="0%"
+            {...getOverrideProps(overrides, "Vector3848790")}
+          ></Icon>
+        </Flex>
+      </Flex>
+      <Flex
+        gap="38px"
+        direction="row"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-end"
+        position="absolute"
+        top="455px"
+        left="calc(50% - 41px - 0px)"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Frame 9")}
+      >
+        <Flex
+          padding="0px 0px 0px 0px"
+          width="22px"
+          height="24.25px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          shrink="0"
+          position="relative"
+          {...getOverrideProps(overrides, '\uD83E\uDD86 icon "trash"')}
+        >
+          <View
+            padding="0px 0px 0px 0px"
+            width="22px"
+            height="24.25px"
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            position="absolute"
+            top="0%"
+            bottom="0%"
+            left="0%"
+            right="0%"
+            {...getOverrideProps(overrides, "Group3848785")}
+          >
+            <Icon
+              width="22px"
+              height="6.18px"
+              viewBox={{ minX: 0, minY: 0, width: 22, height: 6.177734375 }}
+              paths={[
+                {
+                  d: "M21.2198 4.48665C19.4049 4.30628 17.5899 4.17101 15.7637 4.06955L15.7637 4.05828L15.5157 2.59279C15.3466 1.55567 15.0986 0 12.4607 0L9.5072 0C6.88055 0 6.63255 1.48803 6.45218 2.58151L6.21545 4.02446C5.16706 4.0921 4.11867 4.15973 3.07028 4.26119L0.770594 4.48665C0.297128 4.53174 -0.0410613 4.94884 0.00403068 5.41104C0.0491226 5.87323 0.45495 6.21142 0.928416 6.16633L3.22811 5.94087C9.13519 5.35467 15.0873 5.58013 21.062 6.1776L21.1522 6.1776C21.5806 6.1776 21.9526 5.85068 21.9977 5.41104C22.0315 4.94884 21.6933 4.53174 21.2198 4.48665Z",
+                  fill: "rgba(41,45,50,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              position="absolute"
+              top="0%"
+              bottom="74.52%"
+              left="0%"
+              right="0%"
+              {...getOverrideProps(overrides, "Vector3848786")}
+            ></Icon>
+            <Icon
+              width="17.07px"
+              height="16.92px"
+              viewBox={{
+                minX: 0,
+                minY: 0,
+                width: 17.07177734375,
+                height: 16.9208984375,
+              }}
+              paths={[
+                {
+                  d: "M16.6862 0.439647C16.4157 0.157822 16.0437 0 15.6604 0L1.4114 0C1.02811 0 0.644832 0.157822 0.385553 0.439647C0.126275 0.721472 -0.0202744 1.10475 0.00227158 1.49931L0.701197 13.0654C0.8252 14.7789 0.983022 16.9208 4.9173 16.9208L12.1545 16.9208C16.0888 16.9208 16.2467 14.7902 16.3706 13.0654L17.0695 1.51058C17.0921 1.10475 16.9455 0.721472 16.6862 0.439647ZM10.4072 11.273L6.65328 11.273C6.19114 11.273 5.80786 10.8897 5.80786 10.4275C5.80786 9.96532 6.19114 9.58204 6.65328 9.58204L10.4072 9.58204C10.8694 9.58204 11.2527 9.96532 11.2527 10.4275C11.2527 10.8897 10.8694 11.273 10.4072 11.273ZM11.3541 6.76379L5.71768 6.76379C5.25549 6.76379 4.8722 6.38051 4.8722 5.91832C4.8722 5.45613 5.25549 5.07285 5.71768 5.07285L11.3541 5.07285C11.8163 5.07285 12.1996 5.45613 12.1996 5.91832C12.1996 6.38051 11.8163 6.76379 11.3541 6.76379Z",
+                  fill: "rgba(41,45,50,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              position="absolute"
+              top="30.22%"
+              bottom="0%"
+              left="11.18%"
+              right="11.22%"
+              {...getOverrideProps(overrides, "Vector3848787")}
+            ></Icon>
+          </View>
+        </Flex>
+        <Flex
+          padding="0px 0px 0px 0px"
+          width="22px"
+          height="22px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          shrink="0"
+          position="relative"
+          {...getOverrideProps(overrides, '\uD83E\uDD86 icon "send 2"')}
+        >
+          <View
+            padding="0px 0px 0px 0px"
+            width="22px"
+            height="22px"
+            display="block"
+            gap="unset"
+            alignItems="unset"
+            justifyContent="unset"
+            position="absolute"
+            top="0%"
+            bottom="0%"
+            left="0%"
+            right="0%"
+            {...getOverrideProps(overrides, "Group3848793")}
+          >
+            <Icon
+              width="22px"
+              height="22px"
+              viewBox={{ minX: 0, minY: 0, width: 22, height: 22.0029296875 }}
+              paths={[
+                {
+                  d: "M5.30726 3.97025L15.8198 0.462186C20.54 -1.1112 23.104 1.46449 21.5423 6.18465L18.0342 16.6972C15.68 23.7716 11.8106 23.7716 9.45635 16.6972L8.41907 13.5737L5.29561 12.5364C-1.76714 10.1938 -1.76714 6.33616 5.30726 3.97025Z",
+                  fill: "rgba(41,45,50,0.4)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              position="absolute"
+              top="0%"
+              bottom="0%"
+              left="0%"
+              right="0%"
+              {...getOverrideProps(overrides, "Vector3848794")}
+            ></Icon>
+            <Icon
+              width="4.44px"
+              height="4.45px"
+              viewBox={{
+                minX: 0,
+                minY: 0,
+                width: 4.4404296875,
+                height: 4.4521484375,
+              }}
+              paths={[]}
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              position="absolute"
+              top="27.84%"
+              bottom="51.92%"
+              left="50.66%"
+              right="29.16%"
+              {...getOverrideProps(overrides, "Vector3848795")}
+            ></Icon>
+            <Icon
+              width="6.17px"
+              height="6.2px"
+              viewBox={{
+                minX: 0,
+                minY: 0,
+                width: 6.171142578125,
+                height: 6.197265625,
+              }}
+              paths={[
+                {
+                  d: "M0.871189 6.19739C0.649749 6.19739 0.42831 6.11581 0.25349 5.94099C-0.0844966 5.603 -0.0844966 5.04357 0.25349 4.70559L4.68228 0.25349C5.02026 -0.0844966 5.57969 -0.0844966 5.91768 0.25349C6.25566 0.591476 6.25566 1.1509 5.91768 1.48889L1.48889 5.94099C1.31407 6.10415 1.09263 6.19739 0.871189 6.19739Z",
+                  fill: "rgba(41,45,50,1)",
+                  fillRule: "nonzero",
+                },
+              ]}
+              display="block"
+              gap="unset"
+              alignItems="unset"
+              justifyContent="unset"
+              position="absolute"
+              top="23.89%"
+              bottom="47.95%"
+              left="46.71%"
+              right="25.24%"
+              {...getOverrideProps(overrides, "Vector3848796")}
+            ></Icon>
+          </View>
+        </Flex>
+      </Flex>
+    </View>
+  );
+}
