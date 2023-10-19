02b1d29eddbc68aa36635a22ed539e1302f81398
Akame <144577297+AkameDevMgr@users.noreply.github.com>
2023-10-19 09:45:42 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Flex direction={"column"} alignItems={"center"}>
         <Flex direction={"column"}>
+          <Heads />
           
         </Flex>
       </Flex>
